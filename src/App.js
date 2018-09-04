7d8bdd888365bea2bad224d41bc17889e462da24
Zak Weiland <zweiland@Zaks-MBP.attlocal.net>
2018-09-04 10:55:14 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -322,207 +322,216 @@ const Settings = () => (
   </div>
 )
 
-const Client = ({ match }) => (
-  <div className="app">
-    <header className="app-header flex flex-wrap">
-      <div className="w-1/3 app-header-back-button">
-        <Link to="/" className="block">
-          <img src={iconChevronLeft} className="mr-1 float-left" alt="chevron left" />
-          <span>Home</span>
-        </Link>
-      </div>
-      <div className="w-1/3 text-center align-middle">
-        <div className="app-header-title">
-          Referral
+class Client extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header flex flex-wrap">
+          <div className="w-1/3 app-header-back-button">
+            <Link to="/" className="block">
+              <img src={iconChevronLeft} className="mr-1 float-left" alt="chevron left" />
+              <span>Home</span>
+            </Link>
+          </div>
+          <div className="w-1/3 text-center align-middle">
+            <div className="app-header-title">
+              Referral
+            </div>
+          </div>
+        </header>
+
+        <div className="app-banner text-center pt-8 pb-2 mb-8">
+          <h2 className="mb-2">
+            {/* {match.params.clientId} */}
+            Gunny Celis
+          </h2>
+          <h4 className="font-normal text-gray-dark text-sm">
+            Austin, TX
+          </h4>
+          <div className="flex justify-center">
+            <a href="sms:15125551234" className="py-6 px-4 block text-center">
+              <div>
+                <img src={iconMessageCircle} className="h-12 w-12" alt="sms message" />
+              </div>
+              <small className="block text-blue">
+                message
+              </small>
+            </a>
+            <a href="tel:15125551234" className="py-6 px-4 block text-center">
+              <div>
+                <img src={iconPhoneCircle} className="h-12 w-12" alt="" />
+              </div>
+              <small className="block text-blue">
+                call
+              </small>
+            </a>
+          </div>
         </div>
+
+        <main className="container mx-auto px-6 pb-24">
+          <section className="pb-6 mb-6 border-b border-gray-lightest">
+              <header className="mb-6">
+                <h3 className="mb-2">
+                  Progress
+                  <span className="ml-2 text-sm text-gray float-right">(0% complete)</span>
+                </h3>
+                <p className="text-gray-dark text-xs">Which steps have you completed?</p>
+              </header>
+              <div className="">
+                <ul className="list-reset text-center">
+
+                  {/* BEGIN ACTIVE PROGRESS CHECKBOX */}
+                  <li className="btn-stroked active mr-1 mb-3">
+                    <label className="">
+                      <input type="checkbox" value="Talk" className="checkbox float-left _8X2Bp _3TFgA" />
+                      Talk
+                    </label>
+                  </li>
+                  {/* END ACTIVE PROGRESS CHECKBOX */}
+
+                  <li className="btn-stroked mr-1 mb-3">
+                    <label className="">
+                      <input type="checkbox" value="Set" className="checkbox float-left _8X2Bp _3TFgA" />
+                      Set
+                    </label>
+                  </li>
+                  <li className="btn-stroked mr-1 mb-3">
+                    <label className="">
+                      <input type="checkbox" value="Met" className="checkbox float-left _8X2Bp _3TFgA" />
+                      Met
+                    </label>
+                  </li>
+                  <li className="btn-stroked mr-1 mb-3">
+                    <label className="">
+                      <input type="checkbox" value="Offer" className="checkbox float-left _8X2Bp _3TFgA" />
+                      Offer
+                    </label>
+                  </li>
+                  <li className="btn-stroked mr-1 mb-3">
+                    <label className="">
+                      <input type="checkbox" value="Accept" className="checkbox float-left _8X2Bp _3TFgA" />
+                      Accept
+                    </label>
+                  </li>
+                  <li className="btn-stroked mb-3">
+                    <label className="">
+                      <input type="checkbox" value="Transact" className="checkbox float-left _8X2Bp _3TFgA" />
+                      Transact
+                    </label>
+                  </li>
+                </ul>
+              </div>
+            </section>
+
+            <section className="pb-6 mb-6 border-b border-gray-lightest">
+              <header className="mb-4">
+                <h3 className="mb-2">
+                  Rate your relationship
+                </h3>
+                <p className="text-gray-dark text-xs">How are things working out?</p>
+              </header>
+              <form className="">
+                <div className="mb-2">
+                  <label className="text-base input-radio-container">Dead
+                    <input type="radio" className="input-radio mr-2" name="relationship" />
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
+                <div className="mb-2">
+                  <label className="text-base input-radio-container">Very Weak
+                    <input type="radio" className="input-radio mr-2" name="relationship" />
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
+                <div className="mb-2">
+                  <label className="text-base input-radio-container">Weak
+                    <input type="radio" className="input-radio mr-2" name="relationship" />
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
+                <div className="mb-2">
+                  <label className="text-base input-radio-container">Average
+                    <input type="radio" className="input-radio mr-2" name="relationship" />
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
+                <div className="mb-2">
+                  <label className="text-base input-radio-container">Strong
+                    <input type="radio" className="input-radio mr-2" name="relationship" />
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
+                <div className="mb-2">
+                  <label className="text-base input-radio-container">Very Strong
+                    <input type="radio" className="input-radio mr-2" name="relationship" />
+                    <span className="checkmark"></span>
+                  </label>
+                </div>
+              </form>
+            </section>
+
+            <section className="pb-6 mb-6 border-b border-gray-lightest">
+              <header className="mb-4">
+                <h3 className="mb-2">
+                  Projected close month
+                </h3>
+                <p className="text-gray-dark text-xs">
+                  When do you think they’ll close?
+                </p>
+              </header>
+              <div>
+                <select className="input select" placeholder="Select a month...">
+                  <option>Dead</option>
+                  <option>September ’18</option>
+                  <option>October ’18</option>
+                  <option>November ’18</option>
+                  <option>December ’18</option>
+                  <option>January ’19</option>
+                  <option>February ’19</option>
+                  <option>March ’19</option>
+                  <option>April ’19</option>
+                  <option>May ’19</option>
+                  <option>June ’19</option>
+                  <option>July ’19</option>
+                  <option>August ’19</option>
+                  <option>September ’19</option>
+                  <option>October ’19</option>
+                  <option>November ’19</option>
+                  <option>December ’19</option>
+                  <option>n/a</option>
+                  <option>Other</option>
+                </select>
+              </div>
+            </section>
+
+            <section className="pb-6 mb-6">
+              <header className="mb-4">
+                <h3 className="mb-2">
+                  Notes
+                </h3>
+                <p className="text-gray-dark text-xs">
+                  Anything special about this client?
+                </p>
+              </header>
+              <div>
+                <textarea className="input textarea" placeholder="What's the situation?">
+                </textarea>
+              </div>
+            </section>
+
+            <section className="fixed pin-b actions">
+              <div className="container flex items-center px-6 mx-auto">
+                <span className="flex-1 text-xs text-gray-dark">Everything look good?</span>
+                <div className="flex-1">
+                  <button className="btn btn-green">Save</button>
+                </div>
+              </div>
+            </section>
+        </main>
       </div>
-    </header>
-
-    <div className="app-banner text-center pt-8 pb-4 mb-8">
-      <h2 className="mb-2">
-        Gunny Celis
-        {/* {match.params.clientId} */}
-        {match.params.clientName}
-      </h2>
-      <h4 className="font-normal text-gray-dark text-sm">
-        Austin, TX
-      </h4>
-      <div className="flex justify-center">
-        <a href="sms:15125551234" className="py-6 px-4 block text-center">
-          <div>
-            <img src={iconMessageCircle} className="h-12 w-12" alt="sms message" />
-          </div>
-          <small className="block text-blue">
-            message
-          </small>
-        </a>
-        <a href="tel:15125551234" className="py-6 px-4 block text-center">
-          <div>
-            <img src={iconPhoneCircle} className="h-12 w-12" alt="" />
-          </div>
-          <small className="block text-blue">
-            call
-          </small>
-        </a>
-      </div>
-    </div>
-
-    <main className="container mx-auto px-6 pb-24">
-      <section className="pb-6 mb-6 border-b border-gray-lightest">
-          <header className="mb-6">
-            <h3 className="mb-2">
-              Referral Progress
-              <span className="ml-2 text-sm text-gray">(0% complete)</span>
-            </h3>
-            <p className="text-gray-dark text-xs">Check the box if you have completed this step</p>
-          </header>
-          <div className="flex">
-            <div className="flex-1 text-center">
-              <label className="text-base input-radio-container input-radio-lg">Talk
-                <input type="radio" className="input-radio mr-2" name="client-progress" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-            <div className="flex-1 text-center">
-              <label className="text-base input-radio-container input-radio-lg">Set
-                <input type="radio" className="input-radio mr-2" name="client-progress" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-            <div className="flex-1 text-center">
-              <label className="text-base input-radio-container input-radio-lg">Met
-                <input type="radio" className="input-radio mr-2" name="client-progress" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-            <div className="flex-1 text-center">
-              <label className="text-base input-radio-container input-radio-lg">Offer
-                <input type="radio" className="input-radio mr-2" name="client-progress" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-            <div className="flex-1 text-center">
-              <label className="text-base input-radio-container input-radio-lg">Accept
-                <input type="radio" className="input-radio mr-2" name="client-progress" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-            <div className="flex-1 text-center">
-              <label className="text-base input-radio-container input-radio-lg">Transact
-                <input type="radio" className="input-radio mr-2" name="client-progress" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-          </div>
-        </section>
-
-        <section className="pb-6 mb-6 border-b border-gray-lightest">
-          <header className="mb-4">
-            <h3 className="mb-2">
-              Relationship Quality
-            </h3>
-            <p className="text-gray-dark text-xs">How are things working out?</p>
-          </header>
-          <form className="">
-            <div className="mb-2">
-              <label className="text-base input-radio-container">Dead
-                <input type="radio" className="input-radio mr-2" name="relationship" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-            <div className="mb-2">
-              <label className="text-base input-radio-container">Very Weak
-                <input type="radio" className="input-radio mr-2" name="relationship" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-            <div className="mb-2">
-              <label className="text-base input-radio-container">Weak
-                <input type="radio" className="input-radio mr-2" name="relationship" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-            <div className="mb-2">
-              <label className="text-base input-radio-container">Average
-                <input type="radio" className="input-radio mr-2" checked="checked" name="relationship" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-            <div className="mb-2">
-              <label className="text-base input-radio-container">Strong
-                <input type="radio" className="input-radio mr-2" name="relationship" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-            <div className="mb-2">
-              <label className="text-base input-radio-container">Very Strong
-                <input type="radio" className="input-radio mr-2" name="relationship" />
-                <span className="checkmark"></span>
-              </label>
-            </div>
-          </form>
-        </section>
-
-        <section className="pb-6 mb-6 border-b border-gray-lightest">
-          <header className="mb-4">
-            <h3 className="mb-2">
-              Projected Close Month
-            </h3>
-            <p className="text-gray-dark text-xs">
-              When do you think they’ll close?
-            </p>
-          </header>
-          <div>
-            <select className="input select">
-              <option>Dead</option>
-              <option>September ’18</option>
-              <option>October ’18</option>
-              <option>November ’18</option>
-              <option>December ’18</option>
-              <option>January ’19</option>
-              <option>February ’19</option>
-              <option>March ’19</option>
-              <option>April ’19</option>
-              <option>May ’19</option>
-              <option>June ’19</option>
-              <option>July ’19</option>
-              <option>August ’19</option>
-              <option>September ’19</option>
-              <option>October ’19</option>
-              <option>November ’19</option>
-              <option>December ’19</option>
-              <option>n/a</option>
-              <option>Other</option>
-            </select>
-          </div>
-        </section>
-
-        <section className="pb-6 mb-6">
-          <header className="mb-4">
-            <h3 className="mb-2">
-              Notes
-            </h3>
-            <p className="text-gray-dark text-xs">
-              Anything special about this client?
-            </p>
-          </header>
-          <div>
-            <textarea className="input textarea" placeholder="What's the situation?">
-            </textarea>
-          </div>
-        </section>
-
-        <section className="fixed pin-b actions">
-          <div className="container flex items-center px-6 mx-auto">
-            <span className="flex-1 text-xs text-gray-dark">Everything look good?</span>
-            <div className="flex-1">
-              <button className="btn btn-green">Save</button>
-            </div>
-          </div>
-        </section>
-    </main>
-  </div>
-)
+    )
+  }
+}
 
 const Help = () => (
   <div className="app">
