69dd6434a6ea3c517e68151c8f2a18df43abb94a
Zak Weiland <zweiland@Zaks-MacBook-Pro.local>
2018-09-06 09:54:48 -0500

    remove

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ const Home = () => (
     </header>
 
     <div className="text-center pt-16 pb-10 mb-8">
-      <h2 className="mb-2 text-3xl">Morning Matt, what can I help you with?</h2>
+      <h2 className="mb-2 text-3xl">Morning, Matt!</h2>
       <p className="text-gray-dark text-base flex items-start justify-center">
         <img src={weatherPartlySunny} className="mr-2" alt="Partly Sunny" />
         <span className="inline-block">Mostly Sunny 90° F</span>
