b00b9014f421aa9324c38ab7c10a0cd17797b795
Zak Weiland <zweiland@ip-192-168-2-219.ec2.internal>
2018-10-19 10:02:50 -0500

    loading page updates

--- src/scenes/SignIn/Loading.js
+++ src/scenes/SignIn/Loading.js
@@ -58,8 +58,8 @@ class Loading extends React.Component {
       return (
         <div className="app">
           <main className="flex h-screen items-stretch">
-            <div className="self-center flex-1 text-center">
-              <Link to="/" className="ojo-loading">
+            <div className="self-center flex-1">
+              <Link to="/" className="ojo-loading flex mx-auto text-center">
                 <img src={logoOJOWhite} className="flex-1 self-center" alt="logo" />
               </Link>
             </div>
@@ -74,12 +74,14 @@ class Loading extends React.Component {
          <main className="flex h-screen items-stretch">
            <div className="self-center flex-1 text-center">
              <div className="flex items-stretch">
-               <Link to="/" className="ojo-loading">
+              <div className="flex-1 w-1/2 text-right mx-2">
+               <Link to="/" className="ojo-loading flex float-right">
                  <img src={logoOJOWhite} className="flex-1 self-center" alt="logo" />
                </Link>
-               <div className="hi flex-1">
+              </div>
+              <div className="hi w-1/2 flex-1 text-left mx-2">
                 {this.state.data}
-               </div>
+              </div>
              </div>
            </div>
          </main>
