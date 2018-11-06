0feda6ee0ebb5723289717aba1e31f18a4d6f8b6
Zak Weiland <zweiland@ip-192-168-2-219.ec2.internal>
2018-11-06 16:29:33 -0600

    name

--- client/src/scenes/Home/Home_NeedsReview.js
+++ client/src/scenes/Home/Home_NeedsReview.js
@@ -20,7 +20,7 @@ class HomeNeedsReview extends React.Component {
     return (
       <div className="app">
         <AppHeader />
-        <AgentHomeBanner firstName="Jacqueline" temperature="74" />
+        <AgentHomeBanner firstName="Jackie" temperature="74" />
 
         <main className="container mx-auto px-2 max-w-lg">
 
