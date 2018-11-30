dd26fc755a424902e6b5c205650a1bb187da88bb
Zak Weiland <zweiland@Zaks-MBP.attlocal.net>
2018-11-30 08:34:27 -0600

    new snackbar style, fixed header on referrals

--- client/src/scenes/ReferralDetail/Approved/Referral_Approved_Arthur_Profile.js
+++ client/src/scenes/ReferralDetail/Approved/Referral_Approved_Arthur_Profile.js
@@ -6,22 +6,48 @@ import iconChatWhite from '../../../assets/images/icon-chat-w.png';
 import iconPhoneWhite from '../../../assets/images/icon-phone-w.png';
 import iconEmailWhite from '../../../assets/images/icon-email-w.png';
 import iconChevronLeftWhite from '../../../assets/images/icon-chevron-left-w.png';
+import iconChevronLeftBlue from '../../../assets/images/icon-chevron-left.svg';
 import iconNote from '../../../assets/images/icons/icon-note.png';
 
 // Components
 import HomeListItem from '../../../components/HomeListItem/index.js';
 
 class ReferralProfile extends React.Component {
-  componentDidMount() {
-    window.scrollTo(0, 0);
-  }
   constructor(props) {
     super(props);
     this.state = {
       isChecked: props.isChecked || false,
+      referralHeader: null,
     };
 
     this.handleChange = this.handleChange.bind(this);
+  }
+  componentDidMount() {
+    window.scrollTo(0, 0);
+
+    window.addEventListener('scroll', function() {
+      this.referralHeader = document.getElementById("appReferralHeader");
+
+      //console.log('scrolling');
+      //console.log(window.pageYOffset);
+
+      if(window.pageYOffset >= 290 ) {
+        //console.log("show");
+        if (this.referralHeader != null) {
+          this.referralHeader.classList.add("showit");
+        }
+      } else {
+        //console.log("hide");
+        if (this.referralHeader != null) {
+          if (this.referralHeader.classList.contains('showit')) {
+            this.referralHeader.classList.remove("showit");
+          }
+        }
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.referralHeader = null;
   }
   handleChange() {
     this.setState({ isChecked: !this.state.isChecked })
@@ -30,7 +56,7 @@ class ReferralProfile extends React.Component {
     return (
       <div className="app">
         <div className="review-status-wrapper review-status_approved">
-          <header className="h-16 flex">
+          <header className="h-16 flex fixed pin-t w-full">
             <div className="w-1/3 app-header-back-button">
               <Link to="/" className="block">
                 <img src={iconChevronLeftWhite} className="icon icon-chevron-left mr-1 float-left" alt="chevron left" />
@@ -44,7 +70,40 @@ class ReferralProfile extends React.Component {
             */}
           </header>
 
-          <div className="app-banner text-center mb-6">
+          <header className="app-referral-header fixed flex h-16 bg-white w-full pin-t border-b border-grey-lighter" id="appReferralHeader">
+            <div className="w-1/3 app-header-back-button">
+              <Link to="/" className="block">
+                <img src={iconChevronLeftBlue} className="icon icon-chevron-left mr-1 float-left" alt="chevron left" />
+                <span>Home</span>
+              </Link>
+            </div>
+
+            <div className="w-1/3 text-center align-middle">
+              <div className="mt-6">
+                <h4 className="text-black text-lg mb-1">Arthur Belling</h4>
+              </div>
+            </div>
+
+            <div className="w-1/3 flex text-center align-middle">
+              <a href="sms:15125551234" className="py-6 px-4 block text-center text-blue">
+                <div className="btn-contact-icon flex items-center">
+                  <div className="block w-full flex items-center justify-center">
+                    <img src={iconChatWhite} className="w-5" alt="sms message" />
+                  </div>
+                </div>
+              </a>
+              <a href="tel:15125551234" className="py-6 px-4 block text-center text-blue">
+                <div className="btn-contact-icon flex items-center">
+                  <div className="block w-full flex items-center justify-center">
+                    <img src={iconPhoneWhite} className="h-5" alt="" />
+                  </div>
+                </div>
+              </a>
+            </div>
+          </header>
+
+
+          <div className="app-banner text-center mb-6 pt-16">
             <div className="avatar avatar-lg bg-blue-lightest flex items-center">
               <span className="block w-full text-center mt-2">A</span>
             </div>
